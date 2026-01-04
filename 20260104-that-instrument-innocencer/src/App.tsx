42550495bdb3cb9a070ef24d39442f1e6c42b986
mikancode <250844960+mikancode@users.noreply.github.com>
2026-01-05 02:11:40 +0900

    ✨ feat: 変数・型指定・演算・条件分岐・ループを試す

--- 20260104-that-instrument-innocencer/src/App.tsx
+++ 20260104-that-instrument-innocencer/src/App.tsx
@@ -2,12 +2,42 @@ import './App.css'
 
 function App() {
 
+  // const…定数、let…変数
+  const message = "Hello, World!";
+  let subMessage = "Hello...";
+  subMessage = "Hello...!";
+
+  // 変数での型指定
+  const r: number = 100;
+  const pi: number = 3.14;
+  const isActive: boolean = true;
+
+  // 関数での型指定（引数と戻り値）
+  const culc: (a: number, b: number) => number = (a, b) => {
+    // 条件分岐
+    if (isActive) {
+      return 2 * a * b;
+    }
+    else {
+      return (a ** 2) * b;
+    }
+  };
+
+  const loop: (a: number, b: number) => string = (a, b) => {
+    // 繰り返し
+    let result: string = "";
+    for (let i: number = 0; i < b; i++) {
+      result += a.toString();
+    }
+    return result;
+  };
+
   return (
     <div>
-      <h1>hello, World!</h1>
-      <h2>hello...</h2>
-      <h3>hello......</h3>
-      <h4>hello.........</h4>
+      <h1>{message}</h1>
+      <h2>{subMessage}</h2>
+      <h3>{culc(r, pi)}</h3>
+      <h4>{loop(r, pi)}</h4>
     </div>
   )
 }
